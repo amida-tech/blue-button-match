7a066ec36c9d9f99d0e61d55053716058a07624c
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-05-21 02:01:42 -0400

    Partial match for procedures is implemented with tests and samples

--- test/test-partial/test-procedures.js
+++ test/test-partial/test-procedures.js
@@ -8,31 +8,31 @@ var expect = require('chai').expect;
 var fs = require('fs');
 //var bbjs = require('blue-button');
 
-var comparePartial = require('../../lib/sections/flat/problems.js').compare;
+var comparePartial = require('../../lib/sections/flat/procedures.js').compare;
 var matchSections = require("../../lib/match-sections.js").matchSections;
 
 var js, js2, js3, js4;
 
 before(function(done) {
-    // 2 sample problems
-    js = JSON.parse(fs.readFileSync('test/test-partial/fixtures/problems.json', 'utf-8').toString());
+    // 3 sample procedures
+    js = JSON.parse(fs.readFileSync('test/test-partial/fixtures/procedures.json', 'utf-8').toString());
 
     //same as above but rearranged to be partial match
-    js2 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/problems2.json', 'utf-8').toString());
+    js2 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/procedures2.json', 'utf-8').toString());
 
-    // has a bunch of problems different from all of the above
-    js3 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/problems3.json', 'utf-8').toString());
+    // has 1 different procedure from all of the above
+    js3 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/procedures3.json', 'utf-8').toString());
 
     //console.log(bb);
     done();
 });
 
 
-describe('Problems partial matching library (problems.js) tests', function() {
+describe('Procedures partial matching library (problems.js) tests', function() {
 
 
 
-        it('compare two different problems sections', function() {
+        it('compare two different procedures sections', function() {
             var m = matchSections(js, js3, comparePartial);
 
             //console.log(js.length);
@@ -50,7 +50,7 @@ describe('Problems partial matching library (problems.js) tests', function() {
         });
 
 
-        it('compare problems sections with itself', function() {
+        it('compare procedures sections with itself', function() {
             var m = matchSections(js, js, comparePartial);
 
             //console.log(m);
@@ -76,7 +76,7 @@ describe('Problems partial matching library (problems.js) tests', function() {
         });
 
 
-        it('compare two different problems sections that will have all partial match', function() {
+        it('compare two different procedures sections that will have all partial match', function() {
             var m = matchSections(js, js2, comparePartial);
 
             //console.log(m);
@@ -103,7 +103,7 @@ describe('Problems partial matching library (problems.js) tests', function() {
         });
 
 
-        xit('compare two different problems sections that will have some partial match, some dups and some new', function() {
+        xit('compare two different procedures sections that will have some partial match, some dups and some new', function() {
             var result = ["partial", "new", "duplicate"].sort();
 
             var m = matchSections(js4, js4, comparePartial);
