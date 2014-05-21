9a34f23b427c9301da79cb5ed16930b60ee932d5
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-05-21 01:20:23 -0400

    Partial problems match with tests and samples

--- test/test-partial/test-problems.js
+++ test/test-partial/test-problems.js
@@ -8,37 +8,32 @@ var expect = require('chai').expect;
 var fs = require('fs');
 //var bbjs = require('blue-button');
 
-var comparePartial = require('../../lib/sections/flat/medications.js').compare;
+var comparePartial = require('../../lib/sections/flat/problems.js').compare;
 var matchSections = require("../../lib/match-sections.js").matchSections;
 
 var js, js2, js3, js4;
 
 before(function(done) {
-    // medications and medications have same 1 med with rearranged translation
-    // each has 1 med
-    js = JSON.parse(fs.readFileSync('test/test-partial/fixtures/medications.json', 'utf-8').toString());
-    js2 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/medications2.json', 'utf-8').toString());
+    // 2 sample problems
+    js = JSON.parse(fs.readFileSync('test/test-partial/fixtures/problems.json', 'utf-8').toString());
 
-    // medications3 has same med as medication but with same date range with different precision
-    // has 1 med
-    js3 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/medications3.json', 'utf-8').toString());
+    //same as above but rearranged to be partial match
+    js2 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/problems2.json', 'utf-8').toString());
 
-    // has a bunch of meds different from all of the above
-    // has 17 meds (all status=prescribed)
-    // NOTE: actually really bad sample, since meds have UNKNOWN codes
-    js4 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/medications4.json', 'utf-8').toString());
+    // has a bunch of problems different from all of the above
+    js3 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/problems3.json', 'utf-8').toString());
 
     //console.log(bb);
     done();
 });
 
 
-describe('Medications partial matching library (medications.js) tests', function() {
+describe('Problems partial matching library (problems.js) tests', function() {
 
 
 
-        it('compare two different medications sections', function() {
-            var m = matchSections(js, js4, comparePartial);
+        it('compare two different problems sections', function() {
+            var m = matchSections(js, js3, comparePartial);
 
             //console.log(js.length);
             //console.log(js2.length);
@@ -67,7 +62,7 @@ describe('Medications partial matching library (medications.js) tests', function
                 expect(m[item]).to.have.property('dest_id');
             }
 
-            var m = matchSections(js4, js4, comparePartial);
+            var m = matchSections(js3, js3, comparePartial);
 
             //console.log(m);
 
@@ -81,37 +76,8 @@ describe('Medications partial matching library (medications.js) tests', function
         });
 
 
-        it('compare medications sections with itself (but translations in different order)', function() {
+        it('compare two different medications sections that will have all partial match', function() {
             var m = matchSections(js, js2, comparePartial);
-
-            //console.log(js,js2,m)
-
-            for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("partial");
-                expect(m[item]).to.have.property('src_id');
-                expect(m[item]).to.have.property('dest_id');
-            }
-        });
-
-        it('compare medications sections with itself (but ranges of differnt precision)', function() {
-            //console.log(JSON.stringify(js),JSON.stringify(js3));
-
-            var m = matchSections(js, js3, comparePartial);
-
-            //console.log(js,js3,m);
-
-            for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("partial");
-                expect(m[item]).to.have.property('src_id');
-                expect(m[item]).to.have.property('dest_id');
-            }
-        });
-
-
-        xit('compare two different medications sections that will have all partial match', function() {
-            var m = matchSections(js3, js3, comparePartial);
 
             //console.log(m);
 
@@ -123,7 +89,7 @@ describe('Medications partial matching library (medications.js) tests', function
             }
 
             //and now do the same but backwards
-            var m = matchSections(js3, js3.slice().reverse(), comparePartial);
+            var m = matchSections(js, js2.slice().reverse(), comparePartial);
 
             //console.log(m);
 
