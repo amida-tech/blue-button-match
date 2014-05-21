faddff81febc85bc4fd159e64e77d234e4ef100b
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-05-21 18:56:13 -0400

    simple test that .match({},{}) works

---
+++ test/test-index.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+var lookups = require('../lib/lookups.js');
+
+var match= require('../index.js').match;
+
+describe('index.js test', function () {
+
+    it('compare two empty {}', function () {
+        //expect(true).to.equal(true);
+        var m = match({},{});
+        expect(m).to.be.ok;
+    });
+
+
+
+});
+
+
