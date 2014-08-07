ebd4ee5f06d4b95e7dfc1cb9c0926e71423bfa96
Matthew McCall <matt@mattmccall.com>
2014-08-07 10:43:06 -0700

    Immunizations Migrated.

--- test/test-partial/test-immunizations.js
+++ test/test-partial/test-immunizations.js
@@ -4,6 +4,7 @@
 "use strict";
 
 var expect = require('chai').expect;
+var _ = require('underscore');
 
 var fs = require('fs');
 //var bbjs = require('blue-button');
@@ -23,6 +24,8 @@ before(function(done) {
     // has a bunch of immunizations different from all of the above
     js3 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/immunizations3.json', 'utf-8').toString());
 
+    js4 = JSON.parse(fs.readFileSync('test/test-partial/fixtures/immunizations4.json', 'utf-8').toString());
+
     //console.log(bb);
     done();
 });
@@ -33,94 +36,207 @@ describe('Immunizations partial matching library (immunizations.js) tests', func
 
 
         it('compare two different immunizations sections', function() {
-            var m = matchSections(js, js3, comparePartial);
+            var m = matchSections(js, js3, 'immunizations');
 
-            //console.log(js.length);
-            //console.log(js2.length);
-
+            //console.log(JSON.stringify(js3, null, 10));
+            //console.log(JSON.stringify(js, null, 10));
             //console.log(m);
 
+            expect(m.length).to.equal(20);
+            expect(_.where(m, {dest: 'dest'}).length).to.equal(8);
+            expect(_.where(m, {dest: 'src'}).length).to.equal(12);
+
             for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("new");
-                expect(m[item]).to.have.property('src_id');
-                expect(m[item]).to.not.have.property('dest_id');
+                    expect(m[item].match).to.equal("new");
+                    expect(m[item]).to.have.property('src_id');
+                    expect(m[item]).to.have.property('dest_id');
             }
 
         });
 
 
         it('compare immunizations sections with itself', function() {
-            var m = matchSections(js, js, comparePartial);
+            var m = matchSections(js, js, 'immunizations');
 
             //console.log(m);
 
+            //Group arrays by source.
+            var src_array = [];
             for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("duplicate");
-                expect(m[item]).to.have.property('src_id');
-                expect(m[item]).to.have.property('dest_id');
+                src_array.push(m[item].src_id);
             }
 
-            var m = matchSections(js2, js2, comparePartial);
+            src_array = _.uniq(src_array);
+            var src_obj_array = [];
 
-            //console.log(m);
+            for (var i in src_array) {
+                src_obj_array.push([]);
+            }
 
             for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("duplicate");
-                expect(m[item]).to.have.property('src_id');
-                expect(m[item]).to.have.property('dest_id');
+                for (var iter in src_array) {
+                    if (m[item].src_id === src_array[iter]) {
+                        src_obj_array[src_array[iter]].push(m[item]);
+                    }
+
+                }
+            }
+
+            for (var objArray in src_obj_array) {
+                //console.log(src_obj_array[objArray]);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest'}).length).to.equal(4);
+                expect(_.where(src_obj_array[objArray], {dest: 'src'}).length).to.equal(3);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest', match: 'duplicate'}).length).to.equal(1);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest', match: 'new'}).length).to.equal(3);
+                expect(_.where(src_obj_array[objArray], {dest: 'src', match: 'duplicate'}).length).to.equal(0);
+                expect(_.where(src_obj_array[objArray], {dest: 'src', match: 'new'}).length).to.equal(3);
             }
 
         });
 
 
         it('compare two different immunizations sections that will have all partial match', function() {
-            var m = matchSections(js, js2, comparePartial);
+            var m = matchSections(js, js2, 'immunizations');
 
-            //console.log(m);
+            //console.log(JSON.stringify(m,null,4));
+
+            //Group arrays by source.
+            var src_array = [];
+            for (var item in m) {
+                src_array.push(m[item].src_id);
+            }
+
+            src_array = _.uniq(src_array);
+            var src_obj_array = [];
+
+            for (var i in src_array) {
+                src_obj_array.push([]);
+            }
 
             for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("partial");
-                //expect(m[item]).to.have.property('src_id');
-                //expect(m[item]).to.not.have.property('dest_id');
+                for (var iter in src_array) {
+                    if (m[item].src_id === src_array[iter]) {
+                        src_obj_array[src_array[iter]].push(m[item]);
+                    }
+
+                }
+            }
+
+            for (var objArray in src_obj_array) {
+                //console.log(src_obj_array[objArray]);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest'}).length).to.equal(4);
+                expect(_.where(src_obj_array[objArray], {dest: 'src'}).length).to.equal(3);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest', match: 'partial'}).length).to.equal(1);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest', match: 'new'}).length).to.equal(3);
+                expect(_.where(src_obj_array[objArray], {dest: 'src', match: 'partial'}).length).to.equal(0);
+                expect(_.where(src_obj_array[objArray], {dest: 'src', match: 'new'}).length).to.equal(3);
+
+                var partial_array = _.where(src_obj_array[objArray], {dest: 'dest', match: 'partial'});
+
+                for (var i in partial_array) {
+                    //console.log(partial_array[i]);
+                    expect(partial_array[i].percent).to.equal(50);
+                    expect(partial_array[i].diff).to.exist;
+                }
+
             }
 
             //and now do the same but backwards
-            var m = matchSections(js, js2.slice().reverse(), comparePartial);
+            var m = matchSections(js, js2.slice().reverse(), 'immunizations');
 
-            //console.log(m);
+                        //Group arrays by source.
+            var src_array = [];
+            for (var item in m) {
+                src_array.push(m[item].src_id);
+            }
+
+            src_array = _.uniq(src_array);
+            var src_obj_array = [];
+
+            for (var i in src_array) {
+                src_obj_array.push([]);
+            }
 
             for (var item in m) {
-                //console.log(m[item].match);
-                expect(m[item].match).to.equal("partial");
-                //expect(m[item]).to.have.property('src_id');
-                //expect(m[item]).to.not.have.property('dest_id');
+                for (var iter in src_array) {
+                    if (m[item].src_id === src_array[iter]) {
+                        src_obj_array[src_array[iter]].push(m[item]);
+                    }
+
+                }
+            }
+
+            for (var objArray in src_obj_array) {
+                //console.log(src_obj_array[objArray]);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest'}).length).to.equal(4);
+                expect(_.where(src_obj_array[objArray], {dest: 'src'}).length).to.equal(3);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest', match: 'partial'}).length).to.equal(1);
+                expect(_.where(src_obj_array[objArray], {dest: 'dest', match: 'new'}).length).to.equal(3);
+                expect(_.where(src_obj_array[objArray], {dest: 'src', match: 'partial'}).length).to.equal(0);
+                expect(_.where(src_obj_array[objArray], {dest: 'src', match: 'new'}).length).to.equal(3);
+
+                var partial_array = _.where(src_obj_array[objArray], {dest: 'dest', match: 'partial'});
+
+                for (var i in partial_array) {
+                    //console.log(partial_array[i]);
+                    expect(partial_array[i].percent).to.equal(50);
+                    expect(partial_array[i].diff).to.exist;
+                }
+
             }
 
         });
 
 
-        xit('compare two different immunizations sections that will have some partial match, some dups and some new', function() {
-            var result = ["partial", "new", "duplicate"].sort();
-
-            var m = matchSections(js4, js4, comparePartial);
+        it('compare two different immunizations sections that will have some partial match, some dups and some new', function() {
+            var m = matchSections(js, js4, 'immunizations');
             //console.log(m);
 
-            expect(m.length).to.equal(3);
-            var m_result=[m[0].match,m[1].match,m[2].match].sort();
+            //Group arrays by source.
+            var src_array = [];
+            for (var item in m) {
+                src_array.push(m[item].src_id);
+            }
 
-            expect(m_result).to.deep.equal(result);
+            src_array = _.uniq(src_array);
+            var src_obj_array = [];
 
-            //and now do the same but backwards
-            var m = matchSections(js4, js4.slice().reverse(), comparePartial);
+            for (var i in src_array) {
+                src_obj_array.push([]);
+            }
 
-            expect(m.length).to.equal(3);
-            var m_result=[m[0].match,m[1].match,m[2].match].sort();
+            for (var item in m) {
+                for (var iter in src_array) {
+                    if (m[item].src_id === src_array[iter]) {
+                        src_obj_array[src_array[iter]].push(m[item]);
+                    }
 
-            expect(m_result).to.deep.equal(result);
+                }
+            }
+
+            //Match One.
+            expect(_.where(src_obj_array[0], {dest: 'dest'}).length).to.equal(3);
+            expect(_.where(src_obj_array[0], {dest: 'src'}).length).to.equal(3);
+            expect(_.where(src_obj_array[0], {dest: 'dest', match: 'new'}).length).to.equal(2);
+            expect(_.where(src_obj_array[0], {dest: 'src', match: 'new'}).length).to.equal(3);
+            expect(_.where(src_obj_array[0], {dest: 'dest', match: 'partial'}).length).to.equal(1);
+            expect(_.where(src_obj_array[0], {dest: 'dest', match: 'duplicate'}).length).to.equal(0);
+
+            //Match Two.
+            expect(_.where(src_obj_array[1], {dest: 'dest'}).length).to.equal(3);
+            expect(_.where(src_obj_array[1], {dest: 'src'}).length).to.equal(3);
+            expect(_.where(src_obj_array[1], {dest: 'dest', match: 'new'}).length).to.equal(3);
+            expect(_.where(src_obj_array[1], {dest: 'src', match: 'new'}).length).to.equal(3);
+            expect(_.where(src_obj_array[1], {dest: 'dest', match: 'partial'}).length).to.equal(0);
+            expect(_.where(src_obj_array[1], {dest: 'dest', match: 'duplicate'}).length).to.equal(0);
+
+            //Match Three.
+            expect(_.where(src_obj_array[3], {dest: 'dest'}).length).to.equal(3);
+            expect(_.where(src_obj_array[3], {dest: 'src'}).length).to.equal(3);
+            expect(_.where(src_obj_array[3], {dest: 'dest', match: 'new'}).length).to.equal(2);
+            expect(_.where(src_obj_array[3], {dest: 'src', match: 'new'}).length).to.equal(3);
+            expect(_.where(src_obj_array[3], {dest: 'dest', match: 'partial'}).length).to.equal(0);
+            expect(_.where(src_obj_array[3], {dest: 'dest', match: 'duplicate'}).length).to.equal(1);
 
         });
 
