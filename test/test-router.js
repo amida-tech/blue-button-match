84b0efa34a5b16f486b97808cbfe3a622799e022
Matthew McCall <matt@mattmccall.com>
2014-08-08 17:32:29 -0700

    Fix router testing.

--- test/test-router.js
+++ test/test-router.js
@@ -6,7 +6,7 @@ var expect = require('chai').expect;
 var fs = require('fs');
 var bbjs = require('blue-button');
 
-var compare = require('../lib/compare-partial.js').compare;
+var compare = require('../lib/router.js').compare;
 var lookups = require('../lib/lookups.js');
 
 var bb;
@@ -33,7 +33,7 @@ describe('Matching library (compare-partial.js) tests', function() {
     describe('Exceptions test', function() {
         it('testing exceptions', function() {
             var fn = function() {
-                compare("none", "none")({
+                compare({
                     "a": 1
                 }, {
                     "a": 1
@@ -41,43 +41,10 @@ describe('Matching library (compare-partial.js) tests', function() {
             };
             expect(fn).to.
             throw ('one argument is required for compare function');
-            fn = function() {
-                compare()({
-                    "a": 1
-                }, {
-                    "a": 1
-                });
-            };
-            expect(fn).to.
-            throw ('one argument is required for compare function');
-
         });
     });
 
     describe('Entries level tests', function() {
-        it('testing compare method', function() {
-
-            expect(compare("none")({
-                "a": 1
-            }, {
-                "a": 1
-            })).to.have.property("match", "duplicate");
-            expect(compare("none")({
-                "a": 1
-            }, {
-                "a": 2
-            })).to.have.property("match", "new");
-
-            //check that order doesnt matter
-            expect(compare("none")({
-                "a": 1,
-                "b": 2
-            }, {
-                "b": 2,
-                "a": 1
-            })).to.have.property("match", "duplicate");
-
-        });
 
         it('testing compare method with BB.js data', function() {
             //expect(true).to.equal(true);
@@ -89,8 +56,9 @@ describe('Matching library (compare-partial.js) tests', function() {
                 if (bb.hasOwnProperty(name)) {
                     for (var entry in bb[name]) {
                         //console.log(bb.data[name][entry]);
-
-                        expect(compare(name)(bb[name][entry], bb[name][entry])).to.have.property("match", "duplicate");
+                        if (name !== 'demographics') {
+                            expect(compare(bb[name][entry], bb[name][entry], name)).to.have.property("match", "duplicate");
+                        }
                     }
                 }
             }
@@ -106,8 +74,9 @@ describe('Matching library (compare-partial.js) tests', function() {
                 if (bb.hasOwnProperty(name)) {
                     for (var entry in bb2[name]) {
                         //console.log(bb.data[name][entry]);
-
-                        expect(compare(name)(bb2[name][entry], bb2[name][entry])).to.have.property("match", "duplicate");
+                        if (name !== 'demographics') {
+                            expect(compare(bb2[name][entry], bb2[name][entry], name)).to.have.property("match", "duplicate");
+                        }
                     }
                 }
             }
@@ -123,8 +92,9 @@ describe('Matching library (compare-partial.js) tests', function() {
                 if (bb.hasOwnProperty(name)) {
                     for (var entry in bb3[name]) {
                         //console.log(bb.data[name][entry]);
-
-                        expect(compare(name)(bb3[name][entry], bb3[name][entry])).to.have.property("match", "duplicate");
+                        if (name !== 'demographics') {
+                            expect(compare(bb3[name][entry], bb3[name][entry], name)).to.have.property("match", "duplicate");
+                        }
                     }
                 }
             }
@@ -141,8 +111,9 @@ describe('Matching library (compare-partial.js) tests', function() {
                 if (bb4.hasOwnProperty(name)) {
                     for (var entry in bb4[name]) {
                         //console.log(bb2.data[name][entry]);
-
-                        expect(compare(name)(bb4[name][entry], bb4[name][entry])).to.have.property("match", "duplicate");
+                        if (name !== 'demographics') {
+                            expect(compare(bb4[name][entry], bb4[name][entry], name)).to.have.property("match", "duplicate");
+                        }
                     }
                 }
             }
