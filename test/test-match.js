c25f71ab8a5974b2856ac909a13e55a8be3bbcba
Matthew McCall <matt@mattmccall.com>
2014-09-11 11:04:36 -0700

    Remove logger.

--- test/test-match.js
+++ test/test-match.js
@@ -73,8 +73,6 @@ describe('Matching library (match.js) tests', function () {
         it('full record comparison of same cms document', function () {
             var m = match.match(bb4.data, bb4.data);
 
-            console.log(m);
-
             expect(m).to.be.ok;
             expect(m).to.have.property("match");
 
